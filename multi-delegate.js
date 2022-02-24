0d1a9035f415ba4bb613a47986fc474790bdfca6
dimiandre <dimiandre@gmail.com>
2022-02-24 09:54:43 -0800

    add txs

--- multi-delegate.js
+++ multi-delegate.js
@@ -2,17 +2,17 @@ const csv = require("csv-parser");
 const fs = require("fs");
 
 var delegation = []
-fs.createReadStream("delegation_amounts.csv")
+fs.createReadStream("new_delegations.csv")
   .pipe(csv())
   .on("data", (row) => {
-    if (row.delegation_amount == "" || row.delegation_amount == undefined) {
+    if (row.amount == "" || row.amount == undefined) {
       return;
     }
     let obj = {
       "@type": "/cosmos.staking.v1beta1.MsgDelegate",
       delegator_address: "juno190g5j8aszqhvtg7cprmev8xcxs6csra7xnk3n3",
       validator_address: row.address,
-      amount: { denom: "ujuno", amount: (parseInt(row.delegation_amount)*1000000).toString() },
+      amount: { denom: "ujuno", amount: (parseInt(row.amount)*1000000).toString() },
     };
     delegation.push(obj)
   })
